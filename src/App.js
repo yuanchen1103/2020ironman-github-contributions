3fbe7485f233bb127b4e871281f89e474bf3ab60
yuanchen <yuanchen1103@gmail.com>
2019-09-26 23:38:12 +0800

    25th day

--- src/App.js
+++ src/App.js
@@ -1,15 +1,72 @@
 import React, { useState, useCallback } from 'react';
 import axios from 'axios';
+import Modal from 'react-modal';
+import { SketchPicker } from 'react-color';
 import './App.scss';
 
 import { groupDataByYear } from './computeData';
 
 import HeatMapWidget from './components/HeatMapWidget/HeatMapWidget';
 
+import deleteIcon from './assets/delete.svg';
+import addIcon from './assets/add.svg';
+
+Modal.setAppElement('#root');
+
 function App() {
   const [input, setInput] = useState('');
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [colors, setColors] = useState([
+    {
+      color: '#F3F3F3',
+      showPicker: false
+    },
+    {
+      color: '#84B6FD',
+      showPicker: false
+    },
+    {
+      color: '#8E87FA',
+      showPicker: false
+    }
+  ]);
+
+  const addColor = useCallback(() => {
+    setColors([
+      ...colors,
+      {
+        color: '#fafafa',
+        showPicker: false
+      }
+    ]);
+  }, [colors]);
+
+  const deleteColor = useCallback(
+    (index) => {
+      setColors([...colors].filter((e, i) => i !== index));
+    },
+    [colors]
+  );
+
+  const toggleColorPicker = useCallback(
+    (index) => {
+      const newColors = [...colors];
+      newColors[index].showPicker = !colors[index].showPicker;
+      setColors(newColors);
+    },
+    [colors]
+  );
+
+  const updateColor = useCallback(
+    (index, color) => {
+      const newColors = [...colors];
+      newColors[index].color = color.hex;
+      setColors(newColors);
+    },
+    [colors]
+  );
 
   const getData = useCallback(() => {
     if (!input.length) return;
@@ -66,7 +123,7 @@ function App() {
   );
 
   return (
-    <div className="App">
+    <div className="App" id="app">
       <section className="header">
         <h1>GitHub Contributions Heatmap</h1>
         <div className="input-group">
@@ -85,6 +142,13 @@ function App() {
             Generate
           </button>
         </div>
+        <button
+          type="button"
+          className="setting-btn"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Date & Color Settings
+        </button>
       </section>
       <div>
         {!!data &&
@@ -97,6 +161,71 @@ function App() {
           <h5>No Data Found</h5>
         )}
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        style={{
+          content: {
+            background: '#FFFFFF',
+            borderRadius: 9,
+            width: 356,
+            minHeight: 386,
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            overflow: 'visible'
+          },
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }
+        }}
+        onRequestClose={() => setIsModalOpen(false)}
+      >
+        <table style={{ marginTop: 48 }}>
+          <tbody>
+            <tr>
+              <td className="label">Color</td>
+              <td>
+                {colors.map((item, i) => (
+                  <div className="color-group" key={i}>
+                    {item.showPicker && (
+                      <div className="color-picker">
+                        <SketchPicker
+                          color={item.color}
+                          onChangeComplete={(color) => updateColor(i, color)}
+                        />
+                      </div>
+                    )}
+                    <div
+                      className="color-block"
+                      style={{ backgroundColor: item.color }}
+                      onClick={() => toggleColorPicker(i)}
+                    ></div>
+                    <img
+                      src={deleteIcon}
+                      alt=""
+                      onClick={() => deleteColor(i)}
+                    />
+                  </div>
+                ))}
+                {colors.length < 5 && (
+                  <img
+                    src={addIcon}
+                    alt=""
+                    className="add-icon"
+                    onClick={addColor}
+                  />
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal>
     </div>
   );
 }
